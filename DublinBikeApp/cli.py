f66f9131bef9c2bd42c41de2f2c29ce853ae2e39
Isaac Dempsey <idempsey7@gmail.com>
2018-03-12 14:32:46 +0000

    Basic web Scraper added to get JSON data from JCDecaux.

--- DublinBikeApp/cli.py
+++ DublinBikeApp/cli.py
@@ -15,4 +15,4 @@ def main(args=None):
 
 
 if __name__ == "__main__":
-    sys.exit(main())  # pragma: no cover
+    main()
---
+++ DublinBikeApp/scraper.py
@@ -0,0 +1,30 @@
+import requests
+import traceback
+import datetime
+import time
+APIKEY="6ea4678acfb75dfd8022fbb67f5170e2ba8bfcdc"
+CONTRACT="Dublin"
+STATIONS="https://api.jcdecaux.com/vls/v1/stations"
+
+# Code taken from Aonghus's Notes
+
+def write_to_file(text, now):
+    with open("data/bikes_{}".format(now).replace(" ", "_"), "w") as f:
+        f.write(text)
+
+def main():
+    while True:
+        try:
+            now = datetime.datetime.now()
+            r = requests.get(STATIONS, params={"apiKey": APIKEY, "contract": CONTRACT})
+            print(r, now)
+            write_to_file(r.text, now)                
+            time.sleep(5*60)
+        except:
+            print(traceback.format_exc())
+    return
+
+main()
+
+if __name__ == "__scraper__":
+    main()
