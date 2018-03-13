a4004b4d09c2cc3b5c44cc314ab2a249b0be6cb8
Isaac Dempsey <idempsey7@gmail.com>
2018-03-13 15:23:34 +0000

    dbWriter now creates static and dynamic tables.

--- DublinBikeApp/dbWriter.py
+++ DublinBikeApp/dbWriter.py
@@ -10,7 +10,7 @@ import time
 
 URI="dublinbikedb.cm66ft6swiuh.us-west-2.rds.amazonaws.com"
 PORT="3306"
-DB = "dublinbikes"
+DB = "dublinbikedb"
 USER = "dublinbikedb"
 PASSWORD = "password"
 
@@ -19,8 +19,44 @@ engine = create_engine("mysql+mysqldb://{}:{}@{}:{}/{}"
 
 
 sql = """
-CREATE DATABASE IF NOT EXISTS dublinbikes;
+CREATE DATABASE IF NOT EXISTS dublinbikedb;
+
 """
 
-
 engine.execute(sql)
+
+sql = """
+CREATE TABLE IF NOT EXISTS static (
+number INTEGER,
+contract_name VARCHAR(256),
+name VARCHAR(256),
+address VARCHAR(256),
+position_lat REAL,
+position_lng REAL,
+banking INTEGER,
+bonus INTEGER
+)
+"""
+try:
+    res = engine.execute("DROP TABLE IF EXISTS static")
+    res = engine.execute(sql)
+    print(res.fetchall())
+except Exception as e:
+    print(e)
+
+sql = """
+CREATE TABLE IF NOT EXISTS dynamic (
+number INTEGER,
+status VARCHAR(256),
+bike_stands INTEGER,
+available_bike_stands INTEGER,
+available_bikes INTEGER,
+last_update INTEGER
+)
+"""
+try:
+    res = engine.execute(sql)
+    print(res.fetchall())
+except Exception as e:
+    print(e)
+
