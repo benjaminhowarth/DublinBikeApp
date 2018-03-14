73550ccfe16dc18fc1b7e4ad0aa7b47354e69266
Isaac Dempsey <idempsey7@gmail.com>
2018-03-14 15:43:27 +0000

    dbWriter pushes a JSON file from data directory to the RDB static table.

--- DublinBikeApp/dbWriter.py
+++ DublinBikeApp/dbWriter.py
@@ -1,10 +1,11 @@
-import sqlalchemy as sqla
-from sqlalchemy import create_engine
+import sqlalchemy
+from sqlalchemy import *
 import traceback
 import glob
 import os
 import requests
 import time
+import json
 
 # Code taken from Aonghus's Notes
 
@@ -13,50 +14,75 @@ PORT="3306"
 DB = "dublinbikedb"
 USER = "dublinbikedb"
 PASSWORD = "password"
-
+  
 engine = create_engine("mysql+mysqldb://{}:{}@{}:{}/{}"
                        .format(USER, PASSWORD, URI, PORT, DB), echo=True)
 
 
-sql = """
-CREATE DATABASE IF NOT EXISTS dublinbikedb;
+# sql = """
+# CREATE DATABASE IF NOT EXISTS dublinbikedb;
+# 
+# """
+# 
+# engine.execute(sql)
+# 
+# sql = """
+# CREATE TABLE IF NOT EXISTS static (
+# number INTEGER NOT NULL,
+# contract_name VARCHAR(256),
+# name VARCHAR(256),
+# address VARCHAR(256),
+# position_lat REAL,
+# position_lng REAL,
+# banking INTEGER,
+# bonus INTEGER
+# PRIMARY KEY (number)
+# )
+# """
+# try:
+#     res = engine.execute(sql)
+#     print(res.fetchall())
+# except Exception as e:
+#     print(e)
+# 
+# sql = """
+# CREATE TABLE IF NOT EXISTS dynamic (
+# number INTEGER,
+# status VARCHAR(256),
+# bike_stands INTEGER,
+# available_bike_stands INTEGER,
+# available_bikes INTEGER,
+# last_update INTEGER
+# )
+# """
+# try:
+#     res = engine.execute(sql)
+#     print(res.fetchall())
+# except Exception as e:
+#     print(e)
 
-"""
+file = "./data/bikes_2018-03-12_14:18:22.696111"
 
-engine.execute(sql)
+def write_to_db(file):
+    if os.path.isfile(file) == True:
+        myFile = open(file, 'r')
+        myFile = myFile.read()
+        myJson = json.loads(myFile)
+        count = 0
+        while count < len(myJson):
+            write_to_static(myJson[count])
+            count += 1    
+    else:
+        print("Error - file path incorrect.")
 
-sql = """
-CREATE TABLE IF NOT EXISTS static (
-number INTEGER,
-contract_name VARCHAR(256),
-name VARCHAR(256),
-address VARCHAR(256),
-position_lat REAL,
-position_lng REAL,
-banking INTEGER,
-bonus INTEGER
-)
-"""
-try:
-    res = engine.execute("DROP TABLE IF EXISTS static")
-    res = engine.execute(sql)
-    print(res.fetchall())
-except Exception as e:
-    print(e)
+def write_to_static(myJson):
+    metadata = MetaData(bind=engine)
+    static = Table('static', metadata, autoload=True)
 
-sql = """
-CREATE TABLE IF NOT EXISTS dynamic (
-number INTEGER,
-status VARCHAR(256),
-bike_stands INTEGER,
-available_bike_stands INTEGER,
-available_bikes INTEGER,
-last_update INTEGER
-)
-"""
-try:
-    res = engine.execute(sql)
-    print(res.fetchall())
-except Exception as e:
-    print(e)
+    ins = static.insert()
+    ins.execute(number=myJson['number'], contract_name=myJson['contract_name'], name=myJson['name'], 
+                address=myJson['address'], position_lat=myJson['position']['lat'], position_lng=myJson['position']['lng'], 
+                banking=myJson['banking'], bonus=myJson['bonus'])
+        
+write_to_db(file)
 
