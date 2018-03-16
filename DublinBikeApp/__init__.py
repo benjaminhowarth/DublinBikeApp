67db298284fd93967a1ddfbc118c6d5879b70cd5
IsaacDempsey <35760904+IsaacDempsey@users.noreply.github.com>
2018-03-16 12:43:13 +0000

    Update __init__.py

--- DublinBikeApp/__init__.py
+++ DublinBikeApp/__init__.py
@@ -3,5 +3,5 @@
 """Top-level package for Dublin Bike App."""
 
 __author__ = """Isaac Dempsey, Ben Howarth, Gary Cremins"""
-__email__ = 'isaac.dempsey()ucdconnect.ie,@ucdconnect.ie, gary.cremins@ucdconnect.ie'
+__email__ = 'isaac.dempsey()ucdconnect.ie, benjamin.howarth@ucdconnect.ie, gary.cremins@ucdconnect.ie'
 __version__ = '0.1.0'
