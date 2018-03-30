18ee40a741e80d17398cba00acbba1de95f44ed4
IsaacDempsey <35760904+IsaacDempsey@users.noreply.github.com>
2018-03-30 09:03:42 +0100

    Fixing indentation

--- DublinBikeApp/static/script.js
+++ DublinBikeApp/static/script.js
@@ -1,37 +1,37 @@
 // Code from Traversy Media - Google Maps JavaScript API Tutorial
 // https://www.youtube.com/watch?v=Zxf1mnP5zcw
 function initMap() {
-    var dublin = {lat: 53.3484906, lng: -6.2551201};
-    
-    // Create Map
+	var dublin = {lat: 53.3484906, lng: -6.2551201};
+
+	// Create Map
 	var map = new google.maps.Map(document.getElementById('map'), {
-      zoom: 14,
-      center: dublin,
-      mapTypeControl: false
-    });
+	zoom: 14,
+	center: dublin,
+	mapTypeControl: false
+	});
 	
 	function addMarker(args){
 		var marker = new google.maps.Marker({
 			position: args.coords,
 			map: map
 		});
-		
+
 		// Check for content
 		if(args.content){
 			marker.addListener('click', function(){
 				if(infoWindow) 
 					infoWindow.close();
-				
+
 				infoWindow = new google.maps.InfoWindow({
 					content: args.content
 				});
-				
+
 				infoWindow.open(map, marker);
 			});
-			
+
 		}
 	}
-	
+
 	var infoWindow;
 	for(var i = 0; i < markers.length; i++){
 		addMarker(markers[i]);
