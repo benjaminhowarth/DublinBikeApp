ea9f6117f1cc6ade2857d6e345ceda54e099ae5d
creminsg <gary.cremins1@ucdconnect.ie>
2018-04-01 12:34:15 +0100

    Added basic chart displaying hard-coded data

--- DublinBikeApp/static/script.js
+++ DublinBikeApp/static/script.js
@@ -41,6 +41,25 @@ function initMap() {
 		if(infoWindow ) 
 			infoWindow.close();
 	});
+	
+	var chart = c3.generate({
+	    bindto: '#chart',
+	    data: {
+	      columns: [
+	        ['data1', 30, 200, 100, 400, 150, 250],
+	        ['data2', 50, 20, 10, 40, 15, 25]
+	      ]
+	    }
+	});
+	
+	chart.load({
+		columns: [
+			['data1', 300, 100, 250, 150, 300, 150, 500],
+		    ['data2', 100, 200, 150, 50, 100, 250]
+		]
+	});
+	
+	
 }
 
 $(document).ready(function(){
